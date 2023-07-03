f49486625e97ec286c7b2dd4c296bd094ea4e21b
Yilia61 <julyueluo@163.com>
2023-07-03 20:31:31 +0800

    0703

--- mooc/chapter6/part3/bind-demo.js
+++ mooc/chapter6/part3/bind-demo.js
@@ -1,14 +1,5 @@
 // 手写bind 函数
 
-function fn1(a, b, c) {
-  console.log('this是', this);
-  console.log((a, b, c*2));
-  return 'this is fn1'
-}
-
-const fn2 = fn1.bind({x: 100}, 10, 20, 30)
-const res = fn2()
-// console.log(res);
 
 //模拟bind
 Function.prototype.bind1 = function () {
@@ -19,14 +10,21 @@ Function.prototype.bind1 = function () {
   // this 获取完后 数组第一项就不需要了
   const t = args.shift()
 
-  // fn1.bind(...)中的 fn1
+  //  this fn1.bind(...)中的 fn1
   const self = this 
 
   // 返回一个函数
   return function () {
     return self.apply(t, args)
-    
   }
-
-  
 }
+function fn1(a, b, c) {
+  console.log('this是', this);
+  console.log((a, b, c*2));
+  return 'this is fn1'
+}
+const fn2 = fn1.bind({x: 100}, 10, 20, 30)
+const res = fn2()
+console.log(res);
+
+
